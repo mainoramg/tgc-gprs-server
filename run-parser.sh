8f3abc8749c378a9ecea5071afb8386cbae98303
mainor <mainor.miranda@bvmedia.cr>
2019-02-03 18:15:24 -0600

    logs backup added to run scripts

--- run-parser.sh
+++ run-parser.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
-
+#!/bin/bash
 ps -edf | grep GPSParser | grep -v grep | awk '{print $2}' | xargs kill -9 2> /dev/null
-
 cd /home/ec2-user/gprs
-
+now=`date -u +"%Y-%m-%d_%H:%M:%S"`
+gzip parser.out
+mv parser.out.gz /home/ec2-user/backup/logs/gprs/parser_$now.out.gz
+gzip parser.err
+mv parser.err.gz /home/ec2-user/backup/logs/gprs/parser_$now.err.gz
+touch parser.out
+touch parser.err
 export CLASSPATH=.:mysql-connector-java-5.1.47-bin.jar
 rm DB.class
 rm GPSEvent.class
 rm GPSParser.class
-
 javac GPSParser.java
 java GPSParser >> parser.out 2>> parser.err &
-
 tail -f parser.out
--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 ps -edf | grep GPRSServer | awk '{printf " " $2}' | xargs kill
 cd /home/ec2-user/gprs
+now=`date -u +"%Y-%m-%d_%H:%M:%S"`
+gzip log.out
+mv log.out.gz /home/ec2-user/backup/logs/gprs/log_$now.out.gz
+touch log.out
 export CLASSPATH=.:mysql-connector-java-5.1.47-bin.jar
 rm DB.class
 rm ServerThread.class
